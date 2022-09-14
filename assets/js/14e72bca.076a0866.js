1c39e5fdb788464103ef9922d7f2df0254eedece
John Ferguson Smart <john.smart@wakaleo.com>
2022-09-14 07:05:34 +0100

    Deploy website - based on 4abb4ff2740b6453bee2a6be883c4501a50cf991

--- assets/js/14e72bca.076a0866.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkserenity_users_guide=self.webpackChunkserenity_users_guide||[]).push([[59],{9215:e=>{e.exports=JSON.parse('{"permalink":"/documentation/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
--- assets/js/e88ec00c.743040e1.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkserenity_users_guide=self.webpackChunkserenity_users_guide||[]).push([[7170],{6737:e=>{e.exports=JSON.parse('{"allTagsPath":"/documentation/blog/tags","slug":"/documentation/blog/tags/locatorless-elements","name":"locatorless-elements","count":1,"permalink":"/documentation/blog/tags/locatorless-elements"}')}}]);
